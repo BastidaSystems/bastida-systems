8ed4ef41f0dd71f7f4f155dab901801c66440fdb
BastidaRodrigo <exmarquesado@gmail.com>
2026-09-23 19:04:05 -0700

    Agregar enlace Store al menu de navegacion
    - site-header.js: enlace Store entre Home y Start a Project
      (bilingue EN/ES via nav.store). A pedido de Rodrigo.

--- site-header.js
+++ site-header.js
@@ -23,6 +23,7 @@
 
       <nav class="bs-nav site-menu-links" id="bs-primary-nav" aria-label="Primary navigation">
         <a href="index.html" data-nav-page="index.html" data-i18n="nav.home">Home</a>
+        <a href="store.html" data-nav-page="store.html" data-i18n="nav.store">Store</a>
         <a class="bs-nav-cta" href="cotizar.html" data-nav-page="cotizar.html">
           <span data-i18n="nav.getStarted">Start a Project</span>
         </a>
